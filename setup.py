cc801d3e6f5c7815f29df02b6f6ea7ee89fbbc89
Les Aker <me@lesaker.org>
2012-10-26 18:48:17 -0400

    added setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='roller',
+      version='0.1',
+      description='Kernel rolling helper',
+      author='Les Aker',
+      author_email='me@lesaker.org',
+      url= 'https://github.com/akerl/roller',
+      packages=['roller'],
+     )
+
