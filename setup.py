bb5930f50352a8ebd550586aa8b349284a54d835
Les Aker <me@lesaker.org>
2014-02-20 21:28:49 -0500

    add troves

--- setup.py
+++ setup.py
@@ -11,6 +11,15 @@ setup(
     author='Les Aker',
     author_email='me@lesaker.org',
     url='https://github.com/akerl/roller',
+    license='MIT License',
     py_modules=['roller'],
     scripts=['roller.py'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Topic :: System :: Operating System Kernels :: Linux',
+    ],
 )
