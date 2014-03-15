27326d554a5cb1aa63f59cfd68c68a19fdddec86
Les Aker <me@lesaker.org>
2014-03-15 11:37:53 -0400

    add tests

--- test/test_roller.py
+++ test/test_roller.py
@@ -1,7 +1,39 @@
 import unittest
+import nose
 import roller
 
-
 class TestRoller:
+    def test_defaults(self):
+        defaults = roller.get_args([])
+        assert defaults.verbose is False
+        assert defaults.new_version == roller.get_latest_kernel_version()
+        assert defaults.new_revision is None
+        assert defaults.config_version == roller.get_current_kernel_version()
+        assert defaults.config_revision == 'current'
+        assert defaults.skip_install is False
+        assert defaults.build_dir == '/tmp'
+        assert defaults.config_dir is None
+    @nose.tools.raises(SystemExit)
+    def test_version(self):
+        assert roller.get_args(['--version'])
+    def test_arguments(self):
+        assert roller.get_args(['-v']).verbose is True
+        assert roller.get_args(['-k', '1.2.3']).new_version == '1.2.3'
+        assert roller.get_args(['-n', '9']).new_revision == '9'
+        assert roller.get_args(['-c', '1.2.3']).config_version == '1.2.3'
+        assert roller.get_args(['-r', '7']).config_revision == '7'
+        assert roller.get_args(['-s']).skip_install is True
+        assert roller.get_args(['-b', '/foo']).build_dir == '/foo'
+        assert roller.get_args(['-d', '/bar']).config_dir == '/bar'
+    def test_get_latest_kernel_version(self):
+        stable = roller.get_latest_kernel_version(kind='stable')
+        longterm = roller.get_latest_kernel_version(kind='longterm')
+        mainline = roller.get_latest_kernel_version(kind='mainline')
+        assert stable != longterm
+        assert 'rc' in mainline
+    def test_get_current_kernel_version(self):
+        assert len(roller.get_current_kernel_version().split('.')) in [2, 3]
+    def test_get_current_kernel_revision(self):
+        pass # TODO: make this test work on Mac or Linux
     def test_easyroll(self):
-        roller.easy_roll(['-s', '-v'])
+        roller.easy_roll(['-v', '-s'])
