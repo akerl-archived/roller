fd405d5bb64549ad8aec3ccf705b07674b6928c0
Les Aker <me@lesaker.org>
2014-06-27 14:00:39 +0000

    silence patch output if not verbose

--- roller.py
+++ roller.py
@@ -306,7 +306,11 @@ class Kernel(object):
     def patch(self, patch_script):
         os.chdir('{0}/sources/linux-{1}'.format(self.build_dir, self.version))
         self.log('Applying patch: {0}'.format(patch_script))
-        resp = subprocess.call([patch_script])
+        if self.verbose:
+            output = None
+        else:
+            output = subprocess.DEVNULL
+        resp = subprocess.call([patch_script], stdout=output, stderr=output)
         if resp != 0:
             raise EnvironmentError('Command failed: {0}'.format(patch_script))
 
