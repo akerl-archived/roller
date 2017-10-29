4d3463d6bd627e6cf3efb32e85eb6f408796fe1a
Les Aker <me@lesaker.org>
2017-10-28 23:04:21 -0700

    fix tests

--- test/test_roller.py
+++ test/test_roller.py
@@ -8,12 +8,12 @@ class TestRoller:
         defaults = roller.get_args([])
         assert defaults.verbose is False
         assert defaults.new_version == roller.get_latest_kernel_version()
-        assert defaults.new_revision is None
-        assert defaults.config_version == roller.get_current_kernel_version()
-        assert defaults.config_revision == 'current'
+        assert defaults.revision == 'dev'
+        assert defaults.config == 'current'
+        assert defaults.output == 'new'
+        assert defaults.modify == False
         assert defaults.skip_install is False
         assert defaults.build_dir == '/tmp'
-        assert defaults.config_dir is None
 
     @nose.tools.raises(SystemExit)
     def test_version(self):
@@ -21,13 +21,10 @@ class TestRoller:
 
     def test_arguments(self):
         assert roller.get_args(['-v']).verbose is True
-        assert roller.get_args(['-k', '1.2.3']).new_version == '1.2.3'
-        assert roller.get_args(['-n', '9']).new_revision == '9'
-        assert roller.get_args(['-c', '1.2.3']).config_version == '1.2.3'
-        assert roller.get_args(['-r', '7']).config_revision == '7'
+        assert roller.get_args(['1.2.3']).new_version == '1.2.3'
+        assert roller.get_args(['-c', '1.2.3']).config == '1.2.3'
         assert roller.get_args(['-s']).skip_install is True
         assert roller.get_args(['-b', '/foo']).build_dir == '/foo'
-        assert roller.get_args(['-d', '/bar']).config_dir == '/bar'
 
     def test_get_latest_kernel_version(self):
         stable = roller.get_latest_kernel_version(kind='stable')
@@ -35,12 +32,6 @@ class TestRoller:
         mainline = roller.get_latest_kernel_version(kind='mainline')
         assert stable != longterm
         assert 'rc' in mainline
-
-    def test_get_current_kernel_version(self):
-        assert len(roller.get_current_kernel_version().split('.')) > 1
-
-    def test_get_current_kernel_revision(self):
-        assert roller.get_current_kernel_revision() == '0'
 
     def test_easyroll(self):
         arg_sets = []
